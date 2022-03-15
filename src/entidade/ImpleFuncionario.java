66889b695caeddf2baffbcc8636873f5ba6042cd
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-15 11:01:52 -0300

    Adicionando Getters and Setters

--- src/entidade/ImpleFuncionario.java
+++ src/entidade/ImpleFuncionario.java
@@ -16,4 +16,28 @@ public class ImpleFuncionario {
 
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
 }
