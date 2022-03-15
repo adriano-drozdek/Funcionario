858caf7e2a60c22156f4ccdf5d428ea91b029967
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-15 11:00:09 -0300

    Criando entidade e adicionando constructor using fields

---
+++ src/entidade/ImpleFuncionario.java
@@ -0,0 +1,19 @@
+package entidade;
+
+public class ImpleFuncionario {
+
+	private Integer id;
+	private String nome;
+	private Double salario;
+
+	public ImpleFuncionario(Integer id, String nome, Double salario) {
+		this.id = id;
+		this.nome = nome;
+		this.salario = salario;
+	}
+
+	public ImpleFuncionario() {
+
+	}
+
+}
